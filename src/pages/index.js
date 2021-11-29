be283e30b20c28010abc6d60cbc775e89f3f11ee
Fernando <fernandocesarrussiefelip@gmail.com>
2021-11-29 16:49:53 -0300

    removendo aos

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import { Helmet } from 'react-helmet'
 import { Header } from '../components/Header'
 import { Main } from '../components/Main'
 import { Footer } from '../components/Footer'
-import AOS from 'aos'
+
 
 export default function Index() {
 
@@ -13,9 +13,6 @@ export default function Index() {
       <Helmet>
         <title>Ferlip</title>
         <link href="https://media.graphcms.com/OHC7wSqXTfyZhk1hF86r" rel="icon" type="image/x-icon" />
-        <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet"/>
-        <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
-        {AOS.init()}
       </Helmet>
       <Header />
       <Main />
